10e8c5331f9a61708c1a2965e2f9d0f9b3f14e5f
niki-01 <niki.manoledaki@gmail.com>
2019-12-16 15:52:28 +0000

    Raises error if value exceeds max possible value

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,12 +2,21 @@ class Oystercard
     
     attr_reader :balance
 
+    MAX_VALUE = 90
+
     def initialize(balance = 0)
         @balance = balance
     end
 
     def top_up(value)
+        fail 'Error: Maximum limit reached' if exceeds_max?(value)
         @balance += value
+    end
+
+    private
+
+    def exceeds_max?(value)
+        @balance + value >= MAX_VALUE 
     end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,16 +2,19 @@ require 'oystercard'
 
 describe Oystercard do
 
-    describe 'initialize' do
+    describe '#initialize' do
         it 'initializes with a default balance' do
             expect(subject.balance).to eq 0
         end
     end
 
-    describe 'top_up' do
+    describe '#top_up' do
         it 'tops up with the top up value as the argument' do
-            subject.top_up(15)
-            expect(subject.balance).to eq 15
+            expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+        end
+
+        it 'raises an error if the action would take the card beyond the limit' do
+            expect{ subject.top_up 100 }.to raise_error 'Error: Maximum limit reached'
         end
     end
 end
