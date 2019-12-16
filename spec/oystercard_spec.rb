f15e43abe414826a07061a84f45ae066e5bd4a4c
niki-01 <niki.manoledaki@gmail.com>
2019-12-16 15:33:06 +0000

    Tops up with the top up value

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,4 +7,11 @@ describe Oystercard do
             expect(subject.balance).to eq 0
         end
     end
+
+    describe 'top_up' do
+        it 'tops up with the top up value as the argument' do
+            subject.top_up(15)
+            expect(subject.balance).to eq 15
+        end
+    end
 end
