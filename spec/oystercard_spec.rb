f9214b49bb300c493e01a6849c22d8a9735f2ef7
niki-01 <niki.manoledaki@gmail.com>
2019-12-16 15:29:34 +0000

    Initializes with a default balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,10 @@
 require 'oystercard'
 
 describe Oystercard do
-    
+
+    describe 'initialize' do
+        it 'initializes with a default balance' do
+            expect(subject.balance).to eq 0
+        end
+    end
 end
