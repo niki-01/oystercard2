5a432a55a2a35d3da2e58d5184aa1874f2107723
niki-01 <niki.manoledaki@gmail.com>
2019-12-17 14:50:22 +0000

    Refactoring spec folder to include 'let' with variables

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,58 +1,59 @@
 class Oystercard
 
-    attr_reader :balance, :entry_station, :exit_station, :journeys
+  attr_reader :balance, :entry_station, :exit_station, :journeys
 
-    MAX_LIMIT = 90
-    MIN_LIMIT = 1
-    MIN_FARE = 2
+  MAX_LIMIT = 90
+  MIN_LIMIT = 1
+  MIN_FARE = 2
 
-    def initialize(balance = 0)
-        @balance = balance
-        @entry_station = nil
-        @exit_station = nil
-        @journeys = []
-    end
+  def initialize(balance = 0)
+    @balance = balance
+    @entry_station = nil
+    @exit_station = nil
+    @journeys = []
+  end
 
-    def top_up(value)
-        fail "Error: Maximum limit of #{MAX_LIMIT.to_s} reached" if exceeds_max?(value)
-        @balance += value
-    end
+  def top_up(value)
+    fail "Error: Maximum limit of #{MAX_LIMIT.to_s} reached" if exceeds_max?(value)
+    @balance += value
+  end
 
-    def in_journey?
-      @entry_station != nil
-    end
+  def in_journey?
+    !!entry_station
+  end
 
-    def touch_in(location)
-      fail "Not enough credit" if min_amount?
-      @entry_station = location
-    end
+  def touch_in(location)
+    fail "Not enough credit" if min_amount?
+    @entry_station = location
+  end
 
-    def touch_out(location)
-      deduct(MIN_FARE)
-      @exit_station = location
-      record_journey
-      @entry_station = nil
-    end
+  def touch_out(location)
+    @exit_station = location
+    record_journey # Journey class
+    @entry_station = nil
+    deduct(MIN_FARE)
+  end
 
-    def journey_number(number)
-      @journeys[number - 1]
-    end
+  def journey_number(number)
+    @journeys[number - 1] # Journey class
+  end
 
-    private
+  private
 
-    def exceeds_max?(value)
-      @balance + value >= MAX_LIMIT
-    end
+  def exceeds_max?(value)
+    @balance + value >= MAX_LIMIT
+  end
 
-    def min_amount?
-      @balance <= MIN_LIMIT
-    end
+  def min_amount?
+    @balance <= MIN_LIMIT
+  end
 
-    def deduct(value)
-      @balance -= value
-    end
+  def deduct(value)
+    @balance -= value
+  end
 
-    def record_journey
-      @journeys << {:touch_in => @entry_station, :touch_out => @exit_station}
-    end
+  # Journey class
+  def record_journey
+    @journeys << { :touch_in => @entry_station, :touch_out => @exit_station }
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,97 +2,83 @@ require 'oystercard'
 
 describe Oystercard do
 
-    describe '#initialize' do
-        it 'initializes with a default balance' do
-            expect(subject.balance).to eq 0
-        end
+  describe '#initialize' do
+    it 'initializes with a default balance' do
+      expect(subject.balance).to eq 0
+    end
+  end
+
+  describe '#top_up' do
+    it 'tops up with the top up value as the argument' do
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
     end
 
-    describe '#top_up' do
-        it 'tops up with the top up value as the argument' do
-            expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
-        end
+    it 'raises an error if the action would take the card beyond the limit' do
+      expect{ subject.top_up 100 }.to raise_error "Error: Maximum limit of #{Oystercard::MAX_LIMIT} reached"
+    end
+  end
 
-        it 'raises an error if the action would take the card beyond the limit' do
-            expect{ subject.top_up 100 }.to raise_error "Error: Maximum limit of #{Oystercard::MAX_LIMIT.to_s} reached"
-        end
+  describe 'journey status' do
+    it 'shows if card is in journey' do
+      expect(subject.in_journey?).to eq false
+    end
+  end
+
+  describe '#touch_in' do
+    before(:each) {subject.top_up(10)}
+    let(:old_street) {"Old Street"}
+
+    it 'shows card is in journey after touching in' do
+      subject.touch_in(old_street)
+      expect(subject.in_journey?).to eq true
     end
 
-    describe 'journey status' do
-      it 'shows if card is in journey' do
-        expect(subject.in_journey?).to eq false
-      end
+    it 'returns entry station after touch in' do
+      subject.touch_in(old_street)
+      expect(subject.entry_station).to eq old_street
+    end
+  end
+
+  describe '#touch_out' do
+    let(:old_street) {"Old Street"}
+    let(:kings_cross) {"Kings Cross"}
+    before(:each) do 
+      subject.top_up(10)
+      subject.touch_in(old_street)
     end
 
-    describe '#touch_in' do
-      it 'shows card is in journey after touching in' do
-        subject.top_up(10)
-        old_street = double("Old Street")
-        subject.touch_in(old_street)
-        expect(subject.in_journey?).to eq true
-      end
-
-      it 'returns entry station after touch in' do
-          subject.top_up(10)
-          old_street = double("Old Street")
-          subject.touch_in(old_street)
-          expect(subject.entry_station).to eq old_street
-      end
+    it 'shows card is not in journey after touching in then touching out again' do
+      subject.touch_out(kings_cross)
+      expect(subject.in_journey?).to eq false
+    end
+    
+    it 'reduces balance by minimum fare on touch out' do
+      expect{ subject.touch_out(kings_cross) }.to change{ subject.balance }.by (-Oystercard::MIN_FARE)
+    end
+    
+    it 'after touching out, entry station returns nil' do
+      expect{ subject.touch_out(kings_cross) }.to change{ subject.entry_station }.to nil
+    end
+    
+    it 'after touching out, exit station is set' do
+      expect{ subject.touch_out(kings_cross) }.to change{ subject.exit_station }.to kings_cross
+    end
+    
+    it 'journeys is empty by default' do
+      expect(subject.journeys).to eq []
     end
 
-    describe '#touch_out' do
-
-      before(:each) do
-        subject.top_up(10)
-      end
-
-      it 'shows card is not in journey after touching in then touching out again' do
-        old_street = double("Old Street")
-        kings_cross = double("kings_cross")
-        subject.touch_in(old_street)
-        subject.touch_out(kings_cross)
-        expect(subject.in_journey?).to eq false
-      end
-
-      it 'reduces balance by minimum fare on touch out' do
-        old_street = double("Old Street")
-        kings_cross = double("kings_cross")
-        subject.touch_in(old_street)
-        expect{ subject.touch_out(kings_cross) }.to change{ subject.balance }.by (-Oystercard::MIN_FARE)
-      end
-
-      it 'after touching out, entry station returns nil' do
-        old_street = double("Old Street")
-        kings_cross = double("kings_cross")
-        subject.touch_in(old_street)
-        expect{ subject.touch_out(kings_cross) }.to change{ subject.entry_station }.to nil
-      end
-
-      it 'after touching out, exit station is set' do
-        old_street = double("Old Street")
-        kings_cross = double("kings_cross")
-        subject.touch_in(old_street)
-        expect{ subject.touch_out(kings_cross) }.to change{ subject.exit_station }.to kings_cross
-      end
-
-      it 'journeys is empty by default' do
-        expect(subject.journeys).to eq []
-      end
-
-      it 'after touching out, journey is recorded' do
-        old_street = double("Old Street")
-        kings_cross = double("kings_cross")
-        subject.touch_in(old_street)
-        subject.touch_out(kings_cross)
-        expect(subject.journey_number(1)).to eq ({:touch_in => old_street, :touch_out => kings_cross})
-      end
+    it 'after touching out, journey is recorded' do
+      subject.touch_out(kings_cross)
+      expect(subject.journey_number(1)).to eq ({:touch_in => old_street, :touch_out => kings_cross})
     end
+  end
 
-    describe 'minimum value' do
-        it 'raises an error when touching in with a balance less or equal to one' do
-            old_street = double("Old Street")
-            expect{ subject.touch_in(old_street) }.to raise_error "Not enough credit"
-        end
+  describe 'minimum value' do
+    it 'raises an error when touching in with a balance less or equal to one' do
+      old_street = double("Old Street")
+      expect{ subject.touch_in(old_street) }.to raise_error "Not enough credit"
     end
+  end
 
 end
